79f419f492c707fecfa82ed1155cfb8d52a90819
Ravi <lcs2020043@iiitl.ac.in>
2023-12-27 19:52:44 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
 import Header from "./compoents/Header";
 import UserInput from "./compoents/UserInput";
+import Results from "./compoents/Results";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 1000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration > 0;
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return { ...userInput, [inputIdentifier]: +newValue };
+    });
+  }
+
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput handleChange={handleChange} userInput={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a valid duration (in years)</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
--- src/compoents/UserInput.jsx
+++ src/compoents/UserInput.jsx
@@ -1,18 +1,6 @@
 import { useState } from "react";
 
-export default function UserInput() {
-  const [userInput, setUserInput] = useState({
-    initialInvestment: 1000,
-    annualInvestment: 1200,
-    expectedReturn: 6,
-    duration: 10,
-  });
-
-  function handleChange(inputIdentifier, newValue) {
-    setUserInput((prevUserInput) => {
-      return { ...userInput, [inputIdentifier]: newValue };
-    });
-  }
+export default function UserInput({ userInput, handleChange}) {
 
   return (
     <section id="user-input">
